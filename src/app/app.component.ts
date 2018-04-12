6c4d211470cb63678240988e483f7a34605b8b60
Damon Kaswell <gooddamon@gmail.com>
2018-04-11 17:33:15 -0700

    Bugfix for dragging onto invalid targets and remove console.log entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
     dragstart(i:any, event:any) {
-        console.log('dragstart');
         if (event.target.className === 'sortable-item') {
             event.dataTransfer.setData('text/plain', null);
             this.myItems[+i].moving = true;
@@ -16,54 +15,45 @@ export class AppComponent {
     }
 
     dragend(i:any, event:any) {
-        console.log('dragend');
         if (event.target.className === 'sortable-item') {
             this.myItems[+i].moving = false;
         }
     }
 
     dragover(event:any) {
-        console.log('dragover');
         event.preventDefault();
     }
 
     dragenter(i:any, event:any) {
-        console.log('dragenter');
         if (event.target.className === 'sortable-item-background') {
             this.hovering = +i;
         }
     }
 
     dragleave(event:any) {
-        console.log('dragleave');
-        if ( event.target.className == 'sortable-item-background' ) {
+        if ( event.target.className === 'sortable-item-background' ) {
             this.hovering = -1;
         }
     }
 
     drop(i:any, event:any) {
-        console.log('drop');
         event.preventDefault();
-        if ( event.target.className == 'sortable-item-background' ) {
-            console.log(event);
-        }
+        if ( event.target.className === 'sortable-item-background' ) {
+            let old_item = this.myItems.find(item => item.moving);
+            let old_index = this.myItems.findIndex(item => item.moving);
+            let new_index = +i;
 
-        let old_item = this.myItems.find(item => item.moving);
-        let old_index = this.myItems.findIndex(item => item.moving);
-        let new_index = +i;
+            old_item.moving = false;
+            this.hovering = -1;
 
-        old_item.moving = false;
-        this.hovering = -1;
-
-        if (new_index >= this.myItems.length) {
-            let k = new_index - this.myItems.length + 1;
-            while (k--) {
-                this.myItems.push(undefined);
+            if (new_index >= this.myItems.length) {
+                let k = new_index - this.myItems.length + 1;
+                while (k--) {
+                    this.myItems.push(undefined);
+                }
             }
+            this.myItems.splice(new_index, 0, this.myItems.splice(old_index, 1)[0]);
         }
-
-        this.myItems.splice(new_index, 0, this.myItems.splice(old_index, 1)[0]);
-
     }
 
     add() {
